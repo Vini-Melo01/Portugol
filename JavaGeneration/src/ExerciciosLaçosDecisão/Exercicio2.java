7c446539b3a6aefa659266d72af353e21ad1b170
Konaaz <vinicius.melo.rodrigues@hotmail.com>
2021-02-25 13:58:03 -0300

    arrumando exercicio2

--- JavaGeneration/src/ExerciciosLaçosDecisão/Exercicio2.java
+++ JavaGeneration/src/ExerciciosLaçosDecisão/Exercicio2.java
@@ -17,22 +17,22 @@ public class Exercicio2 {
 		System.out.println("Escreva o terceiro numero: ");
 		numero3= ler.nextDouble();
 		
-		if(numero1>numero2 && numero3<numero2) {
+		if(numero1>=numero2 && numero3<=numero2) {
 			System.out.printf("A sequencia decrescente é : %f, %f, %f",numero1,numero2,numero3);
 		}
-		else if (numero1>numero3 && numero2<numero3) {
+		else if (numero1>=numero3 && numero2<=numero3) {
 			System.out.printf("A sequencia decrescente é : %f, %f, %f",numero1,numero3,numero2);
 		}
-		else if (numero2>numero1 && numero3<numero1) {
+		else if (numero2>=numero1 && numero3<=numero1) {
 			System.out.printf("A sequencia decrescente é : %f, %f, %f",numero2,numero1,numero3);
 		}
-		else if(numero2>numero3 && numero1<numero3) {
+		else if(numero2>=numero3 && numero1<=numero3) {
 			System.out.printf("A sequencia decrescente é : %f, %f, %f",numero2,numero3,numero1);
 		}
-		else if(numero3>numero1 && numero2<numero1) {
+		else if(numero3>=numero1 && numero2<=numero1) {
 			System.out.printf("A sequencia decrescente é : %f, %f, %f",numero3,numero1,numero2);
 		}
-		else if(numero3>numero2 && numero1<numero2) {
+		else if(numero3>=numero2 && numero1<=numero2) {
 			System.out.printf("A sequencia decrescente é : %f, %f, %f",numero3,numero2,numero1);
 		}
 		
