503e164c420a17ac37f21e21c6b0862d86db8dcb
Konaaz <vinicius.melo.rodrigues@hotmail.com>
2021-02-26 15:47:59 -0300

    exercicio 4 ainda ta errado

--- JavaGeneration/src/Funções/Exercicio4While.java
+++ JavaGeneration/src/Funções/Exercicio4While.java
@@ -6,8 +6,8 @@ public class Exercicio4While {
 	public static void main(String[] args) {
 		Scanner leia = new Scanner(System.in);
 		int idade;
-		int sexo ;
-		int temperamento;
+		int sexo = 0 ;
+		int temperamento = 0;
 		int pessoas = 0;
 		int contador = 0;
 		int contador2 = 0;
@@ -22,8 +22,11 @@ public class Exercicio4While {
 		idade = leia.nextInt();
 		System.out.println("Qual o seu sexo? 1-Feminino / 2-Masculino / 3-Outros: ");
 		sexo = leia.nextInt();
+		System.out.printf("Você se considera um(a): %S",sexo(sexo));
 		System.out.println("Você se considera uma pessoa:? 1-Calma / 2-Nervosa / 3-Agressiva: ");
 		temperamento = leia.nextInt();
+		System.out.printf("Você se considera uma pessoa: %S",temperamento(temperamento));
+		temperamento(temperamento);
 		if(temperamento ==1) {
 			contador++;
 		}
@@ -54,50 +57,51 @@ public class Exercicio4While {
 	}
 	public static String sexo(int sexo) {
 		String genero = "";
-		
 		if(sexo==1) {
-			genero = ("Sra.");
+			genero = ("Sra.\n");
 		}
 		
 		else if (sexo==2) {
-			genero = ("Sr");
+			genero = ("Sr.\n");
 
 		}
 		
 		else if (sexo==3) {
-			genero = ("Srx");
+			genero = ("Srx.\n");
 
 		}
 		
 		else {
-			genero = ("N/A, Digite um sexo válido 1-Feminino / 2-Masculino / 3-Outros: ");
+			genero = ("N/A, Digite um sexo válido 1-Feminino / 2-Masculino / 3-Outros: \n");
 		}
+
 		
 		return genero;
 		
 	}
 	public static String temperamento(int temperamento) {
 		String mental = "";
-		
 		if(temperamento==1) {
-			mental = ("Calma");
+			mental = ("Calma\n");
 		}
 		
 		else if(temperamento==2) {
-			mental = ("Nervosa");
+			mental = ("Nervosa\n");
 
 		}
 		
 		else if(temperamento==3) {
-			mental = ("Agressiva");
+			mental = ("Agressiva\n");
 
 		}
-		
 		else {
-			mental = ("N/A, Digite um temperamento válido 1-Calma / 2-Nervosa / 3-Agressiva: ");
+			mental = ("N/A, Digite um temperamento válido 1-Calma / 2-Nervosa / 3-Agressiva: \n");
 		}
+		
 		
 		return mental;
 		
 	}
-}
+	
+	}
+
